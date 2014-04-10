9f13d04c21bada7e2ad7c057de128914e10f533f
Jens Thomsen <dnamesnej@gmail.com>
2014-04-10 13:19:35 +0200

    NOTHING HAPPENED

--- DungerMan/Assets/Scripts/EnemyMovement.cs
+++ DungerMan/Assets/Scripts/EnemyMovement.cs
@@ -2,17 +2,33 @@
 using System.Collections;
 
 public class EnemyMovement : MonoBehaviour {
-	private NavMeshAgent agent;
+	//private NavMeshAgent agent;
+	GameObject player = GameObject.Find ("Cube");
+	GameObject player2 = GameObject.Find ("Cylinder");
+
+
+	float dist = 0;
+
 	void Start() {
-		agent = GetComponent<NavMeshAgent>();
+		dist = Vector2.Distance(player.transform.position, player2.transform.position);
+
+	//agent = GetComponent<NavMeshAgent>();
+
 	}
 	void Update() {
-		RaycastHit hit;
+
+
+
+		print (dist);
+		//RaycastHit hit;
+
+			//agent.SetDestination(player.transform.position);
+		/*
 		if (Input.GetMouseButtonDown(0)) {
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 			if (Physics.Raycast(ray, out hit))
-				agent.SetDestination(hit.point);
+				print (hit.point);
 			
-		}
+		}*/
 	}
 }
--- DungerMan/Assets/Scripts/EnemySpawn.cs
+++ DungerMan/Assets/Scripts/EnemySpawn.cs
@@ -5,13 +5,13 @@ public class EnemySpawn : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		GameObject brah = Instantiate(Resources.Load("Posh")) as GameObject; 
+
 
 		Vector3 temp = transform.position; // copy to an auxiliary variable...
 		temp.y = 5.0f; // modify the component you want in the variable...
 		temp.x = 5.0f;
-
-		brah.transform.position = temp; // and save the modified value 
+		GameObject posh = Instantiate(Resources.Load("Posh")) as GameObject; 
+		posh.transform.position = temp; // and save the modified value 
 
 	}
 	
