01a16f95b33643b43dac3e7e331e5ea9b5b2204c
IAmTipsy <Simon@h451.aau1x-2.klient.sydhavn2.site.aau.dk>
2014-04-21 10:51:44 +0200

    NOIdea

---
+++ DungerMan/Assets/Scripts/PoshScript.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PoshScript : Enemy {
+
+
+	PlayerScript cc;
+
+	public override void ability ()
+	{
+		Debug.Log ("hello");
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		cc = GameObject.Find ("Player(Clone)").GetComponent<PlayerScript>();
+		player = GameObject.Find ("Player(Clone)");
+		agent = GetComponent<NavMeshAgent>();
+
+		Health = 100;
+		Damage = 30;
+		Speed = 50;
+		AttackRange = 2;
+		AttackSpeed = 1;
+		SeeRange = 5;
+		canAttack = false;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		dist = Vector3.Distance(this.transform.position, player.transform.position);
+		autoAttack ();
+
+		//print (canAttack);
+
+
+		if (canAttack) {
+			//cc.pHealth -= 10;
+			EnemyAttack(ref cc.pHealth);
+			
+		}
+
+
+		//print (dist);
+		//print ("posh: "+Health);
+	
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		// if the player collides with the key, following triggers:
+		if (col.gameObject.name == "Player(Clone)") 
+		{
+			takeDamage(50);
+
+		}
+		
+	}
+
+
+
+	IEnumerator diee(){
+		yield return new WaitForSeconds(5);
+		die();
+	}
+	
+}
