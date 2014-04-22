998b796317d53c8e10b4571ba9982207b26daf7f
IAmTipsy <Simon@h451.aau1x-2.klient.sydhavn2.site.aau.dk>
2014-04-22 13:25:38 +0200

    Rock gate
    A gate that destroys itself upon activation allowing selected progress
    in the game

---
+++ DungerMan/Assets/RockGate.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RockGate : MonoBehaviour {
+	ParticleSystem particle;
+	// Use this for initialization
+	void Start () {
+		particle = GameObject.Find("Particle System").GetComponent<ParticleSystem>();
+		particle.playOnAwake = false;
+		particle.Stop();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKeyUp("f"))
+		{
+			Explosion();
+		}
+
+	}
+
+	void Explosion()
+	{
+		particle.Play();
+		print ("big balls");
+		Invoke("SelfDestroy", 4f);
+	}
+
+	void SelfDestroy()
+	{
+		Destroy(gameObject);
+	}
+}
