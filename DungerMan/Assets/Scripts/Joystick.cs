7b7a247e0cd55166ca610c3f2dc7fd3af0f846a5
ChoosingOwl <lka12@student.aau.dk>
2014-04-22 12:21:19 +0200

    Joystick Finally complete
    Joystick Finally complete

--- DungerMan/Assets/Scripts/Joystick.cs
+++ DungerMan/Assets/Scripts/Joystick.cs
@@ -2,8 +2,8 @@
 using System.Collections;
 
 public class Joystick : TouchButtonLogic {
-
-
+	
+	
 	public Transform player = null;
 	public float playerSpeed = 2.0f;
 	public float maxJoyDelta = 0.05f;
@@ -11,74 +11,74 @@ public class Joystick : TouchButtonLogic {
 	private Vector3 joyDelta; // Maximum position of joystick
 	private Transform joyTrans = null; // to move the joystick
 	public CharacterController troller;
-
-
+	
+	
 	void Start () 
 	{
-	
+		
 		joyTrans = this.transform;
-		oJoyPos = joyDelta.position;
-
-
+		oJoyPos = joyTrans.position;
+		
+		
 	}
-
+	
 	void OnTouchBegan ()
 	{
 		//need to cache the touch index that started the joystick
-		touch2Watch = TouchLogic.currTouch;
-
+		touch2Watch = TouchButtonLogic.currTouch;
+		
 	}
-
+	
 	void OnTouchMovedAnywhere ()
 	{
-		if (TouchLogic.currTouch == touch2Watch) 
+		if (TouchButtonLogic.currTouch == touch2Watch) 
 		{
-
+			
 			//move the joystick
 			joyTrans.position = MoveJoyStick ();
 			ApplyJoyDelta ();
-
+			
 		}
 	}
-
+	
 	void OnTouchStayedAnywhere ()
 	{
-		if (TouchLogic.currTouch == touch2Watch) 
+		if (TouchButtonLogic.currTouch == touch2Watch) 
 		{
-		
+			
 			ApplyJoyDelta ();
-
+			
 		}
 	}
 	void OnTouchEndedAnywhere ()
 	{
-		if (TouchLogic.currTouch == touch2Watch) 
+		if (TouchButtonLogic.currTouch == touch2Watch) 
 		{
-
+			
 			//move the joystick back to its origin position
 			joyTrans.position = oJoyPos;
 			touch2Watch = 64;
-
+			
 		}
 	}
-
+	
 	void ApplyJoyDelta ()
 	{
 		troller.Move ((player.forward * joyDelta.y + player.right * joyDelta.x) * playerSpeed * Time.deltaTime);
 	}
-
+	
 	Vector3 MoveJoyStick() //This limits the positioning of the joystick
 	{
 		float x = Input.GetTouch (touch2Watch).position.x / Screen.width;
 		float y = Input.GetTouch (touch2Watch).position.y / Screen.height;
-
-		Vector3 position = new Vector3(Mathf.Clamp(x, oJoyPos.x - maxJoyDelta, oJoyPos.x + maxJoyDelta), 
-			Mathf.Clamp(y, oJoyPos.y - maxJoyDelta, oJoyPos.y + maxJoyDelta), 0);
-
+		
+		Vector3 position = new Vector3 (Mathf.Clamp(x, oJoyPos.x - maxJoyDelta, oJoyPos.x + maxJoyDelta),
+		                                Mathf.Clamp(y, oJoyPos.y - maxJoyDelta, oJoyPos.y + maxJoyDelta), 0);
+		
 		//connecting the joystick with the player, making him move through the joystick
 		joyDelta = new Vector3 (position.x - oJoyPos.x, position.y - oJoyPos.y, 0).normalized;
-
-			return position;
+		
+		return position;
 	}
-
+	
 }
