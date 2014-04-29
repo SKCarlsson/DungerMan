8520820eee348ed1a8480f74b9dc78501c9c758a
Jens Thomsen <dnamesnej@gmail.com>
2014-04-29 11:33:23 +0200

    Deleted some stuff

--- DungerMan/Assets/Scripts/NetworkManager.cs
+++ DungerMan/Assets/Scripts/NetworkManager.cs
@@ -215,20 +215,10 @@ public class NetworkManager : MonoBehaviour {
 			GUI.Box(new Rect(Screen.width/2-250,Screen.height/2-350,500,160),"Choose Role:");
 			if(GUI.Button(new Rect(Screen.width/2-250,Screen.height/2-100,500,160),"Warrior")){
 				SpawnPlayer();
-<<<<<<< HEAD
-				EnemySpawn ();
-=======
-				EnemySpawn();
->>>>>>> FETCH_HEAD
 				player1init = true;
 			}
 			if(GUI.Button(new Rect(Screen.width/2-250,Screen.height/2+80,500,160),"Wizzard")){
 				SpawnPlayer2();
-<<<<<<< HEAD
-				EnemySpawn ();
-=======
-				EnemySpawn();
->>>>>>> FETCH_HEAD
 				player1init = true;
 			}
 		}
@@ -240,12 +230,12 @@ public class NetworkManager : MonoBehaviour {
 			GUI.Box (new Rect (Screen.width / 2 - 250, Screen.height / 2 - 350, 500, 160), "Choose Role:");
 			if (GUI.Button (new Rect (Screen.width / 2 - 250, Screen.height / 2 - 100, 500, 160), "Warrior")) {
 				SpawnPlayer ();
-				//EnemySpawn ();
+				EnemySpawn ();
 				player2init = true;
 			}
 			if (GUI.Button (new Rect (Screen.width / 2 - 250, Screen.height / 2 + 80, 500, 160), "Wizzard")) {
 				SpawnPlayer2 ();
-				//EnemySpawn ();
+				EnemySpawn ();
 				player2init = true;
 			}
 						
